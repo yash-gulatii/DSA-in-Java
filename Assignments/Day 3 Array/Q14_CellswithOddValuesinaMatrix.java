0f2b60ed1d4b9d95d78212b0e8350551d4706816
Yash <contactyashgulati@gmail.com>
2022-11-10 17:47:35 +0530

    Day 8 Searching in 2D Array

---
+++ Assignments/Day 3 Array/Q14_CellswithOddValuesinaMatrix.java
@@ -0,0 +1,14 @@
+public class Q14_CellswithOddValuesinaMatrix {
+    public static void main(String[] args) {
+        int m = 2;
+        int n = 3;
+        int[][] indices = {{0,1},{1,1}};
+    }
+
+    static int oddCells(int m, int n, int[][] indices){
+        int[][] matrix = new int[m][n];
+
+
+        return 1;
+    }
+}
